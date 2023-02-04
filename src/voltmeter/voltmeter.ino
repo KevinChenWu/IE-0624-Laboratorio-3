507b91e4241ae3ef6f05d6391cc1062818cc72f9
KevinChenWu <hongwenchen.k.c.w.90@gmail.com>
2023-02-04 12:39:18 -0600

    Implementación de lectura digital en el firmware

--- src/voltmeter/voltmeter.ino
+++ src/voltmeter/voltmeter.ino
@@ -1,9 +1,14 @@
 int v0, v1, v2, v3;
+int m0, m1, m2, m3;
 
 void setup() {
   // put your setup code here, to run once:
   analogReference(DEFAULT);
   Serial.begin(9600);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
 }
 
 void loop() {
@@ -12,9 +17,29 @@ void loop() {
   v1 = analogRead(A1);
   v2 = analogRead(A2);
   v3 = analogRead(A3);
-  Serial.println("V1: " + String(v0));
-  Serial.println("V2: " + String(v1));
-  Serial.println("V3: " + String(v2));
-  Serial.println("V4: " + String(v3));
+  m0 = digitalRead(A4);
+  m1 = digitalRead(A5);
+  m2 = digitalRead(2);
+  m3 = digitalRead(3);
+  if (m0) {
+    Serial.println("VAC1: " + String(v0));
+  } else {
+    Serial.println("VDC1: " + String(v0));
+  }
+  if (m1) {
+    Serial.println("VAC2: " + String(v1));
+  } else {
+    Serial.println("VDC2: " + String(v1));
+  }
+  if (m2) {
+    Serial.println("VAC3: " + String(v2));
+  } else {
+    Serial.println("VDC3: " + String(v2));
+  }
+  if (m3) {
+    Serial.println("VAC4: " + String(v3));
+  } else {
+    Serial.println("VDC4: " + String(v3));
+  }
   delay(500);
 }
