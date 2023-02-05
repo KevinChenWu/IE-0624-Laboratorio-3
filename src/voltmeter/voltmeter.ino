3e955e2653b089d8484db4ff6731532fd6201759
KevinChenWu <hongwenchen.k.c.w.90@gmail.com>
2023-02-05 17:08:36 -0600

    Implementación del display en el firmware, ajustes en los mensajes a mostrar por el monitor Serial

--- src/voltmeter/voltmeter.ino
+++ src/voltmeter/voltmeter.ino
@@ -1,8 +1,14 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <SPI.h>
+
 const uint8_t n = 64;
 volatile uint16_t s0[n], s1[n], s2[n], s3[n];
 volatile uint8_t s;
 volatile uint16_t os0, os1, os2, os3;
 volatile uint8_t d0, d1, d2, d3;
+
+Adafruit_PCD8544 display = Adafruit_PCD8544(13, 12, 11, 10, 9);
 
 ISR(TIMER1_COMPA_vect) {
   if (s < n) {
@@ -32,6 +38,11 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(3, INPUT);
 
+  Serial.println("V1 V2 V3 V4");
+
+  display.begin();
+  display.clearDisplay();
+
   TCCR1A = 0x00;
   TCCR1B = 0 | (1 << WGM12) | (1 << CS10);
   TCCR1C = 0x00;
@@ -51,69 +62,7 @@ void loop() {
   m1 = digitalRead(A5);
   m2 = digitalRead(2);
   m3 = digitalRead(3);
-
-  // TCNT1 = 0;
-  // TIMSK1 |= (1 << OCIE1A);
-  // while (s < n);
-  // d0 = 1;
-  // s = 0;
-  // while (s < n);
-  // d1 = 1;
-  // s = 0;
-  // while (s < n);
-  // d2 = 1;
-  // s = 0;
-  // while (s < n);
-  // d3 = 1;
-  // s = 0;
-  // TIMSK1 &= ~(1 << OCIE1A);
-  // dc_os0 = os0/n;
-  // dc_os1 = os1/n;
-  // dc_os2 = os2/n;
-  // dc_os3 = os3/n;
-  // for (uint8_t i = 0; i < n; i++) {
-  //   float a0 = s0[i] * 5.0000/1023;
-  //   a0 -= (dc_os0 * 5.0000/1023);
-  //   v0 += a0 * a0;
-  //   float a1 = s1[i] * 5.0000/1023;
-  //   a1 -= (dc_os1 * 5.0000/1023);
-  //   v1 += a1 * a1;
-  //   float a2 = s2[i] * 5.0000/1023;
-  //   a2 -= (dc_os2 * 5.0000/1023);
-  //   v2 += a2 * a2;
-  //   float a3 = s3[i] * 5.0000/1023;
-  //   a3 -= (dc_os3 * 5.0000/1023);
-  //   v3 += a3 * a3;
-  // }
-  // v0 /= n;
-  // v1 /= n;
-  // v2 /= n;
-  // v3 /= n;
-  // Serial.println("v0 " + String(v0));
-  // v0 = sqrt(v0);
-  // v1 = sqrt(v1);
-  // v2 = sqrt(v2);
-  // v3 = sqrt(v3);
-  // if (m0) {
-  //   Serial.println("VAC1: " + String(v0, 4));
-  // } else {
-  //   Serial.println("VDC1: " + String(v0, 4));
-  // }
-  // if (m1) {
-  //   Serial.println("VAC2: " + String(v1, 4));
-  // } else {
-  //   Serial.println("VDC2: " + String(v1, 4));
-  // }
-  // if (m2) {
-  //   Serial.println("VAC3: " + String(v2, 4));
-  // } else {
-  //   Serial.println("VDC3: " + String(v2, 4));
-  // }
-  // if (m3) {
-  //   Serial.println("VAC4: " + String(v3, 4));
-  // } else {
-  //   Serial.println("VDC4: " + String(v3, 4));
-  // }
+  display.clearDisplay();
   if (m0) {
     TCNT1 = 0;
     TIMSK1 |= (1 << OCIE1A);
@@ -128,13 +77,21 @@ void loop() {
     }
     v0 /= n;
     v0 = sqrt(v0) * 48/5;
-    Serial.println("VAC1:" + String(v0, 2));
+    Serial.print(String(v0, 2) + "Vac ");
+    display.setCursor(0, 0);
+    display.print("V1: ");
+    display.print(String(v0, 2));
+    display.print(" Vac");
   } else {
     d0 = 1, s = 0;
     TIMSK1 &= ~(1 << OCIE1A);
     v0 = analogRead(A0) * 5.0000/1023;
     v0 = v0 * 48/5 - 24;
-    Serial.println("VDC1:" + String(v0, 2));
+    Serial.print(String(v0, 2) + "Vdc ");
+    display.setCursor(0, 0);
+    display.print("V1: ");
+    display.print(String(v0, 2));
+    display.print(" Vdc");
   }
   if (m1) {
     TCNT1 = 0;
@@ -150,13 +107,21 @@ void loop() {
     }
     v1 /= n;
     v1 = sqrt(v1) * 48/5;
-    Serial.println("VAC2:" + String(v1, 2));
+    Serial.print(String(v1, 2) + "Vac ");
+    display.setCursor(0, 12);
+    display.print("V2: ");
+    display.print(String(v1, 2));
+    display.print(" Vac");
   } else {
     d1 = 1, s = 0;
     TIMSK1 &= ~(1 << OCIE1A);
     v1 = analogRead(A1) * 5.0000/1023;
     v1 = v1 * 48/5 - 24;
-    Serial.println("VDC2:" + String(v1, 2));
+    Serial.print(String(v1, 2) + "Vdc ");
+    display.setCursor(0, 12);
+    display.print("V2: ");
+    display.print(String(v1, 2));
+    display.print(" Vdc");
   }
   if (m2) {
     TCNT1 = 0;
@@ -172,13 +137,21 @@ void loop() {
     }
     v2 /= n;
     v2 = sqrt(v2) * 48/5;
-    Serial.println("VAC3:" + String(v2, 2));
+    Serial.print(String(v2, 2) + "Vac ");
+    display.setCursor(0, 24);
+    display.print("V3: ");
+    display.print(String(v2, 2));
+    display.print(" Vac");
   } else {
     d2 = 1, s = 0;
     TIMSK1 &= ~(1 << OCIE1A);
     v2 = analogRead(A2) * 5.0000/1023;
     v2 = v2 * 48/5 - 24;
-    Serial.println("VDC3:" + String(v2, 2));
+    Serial.print(String(v2, 2) + "Vdc ");
+    display.setCursor(0, 24);
+    display.print("V3: ");
+    display.print(String(v2, 2));
+    display.print(" Vdc");
   }
   if (m3) {
     TCNT1 = 0;
@@ -194,13 +167,22 @@ void loop() {
     }
     v3 /= n;
     v3 = sqrt(v3) * 48/5;
-    Serial.println("VAC4:" + String(v3, 2));
+    Serial.println(String(v3, 2) + "Vac");
+    display.setCursor(0, 36);
+    display.print("V4: ");
+    display.print(String(v3, 2));
+    display.print(" Vac");
   } else {
     d3 = 1, s = 0;
     TIMSK1 &= ~(1 << OCIE1A);
     v3 = analogRead(A3) * 5.0000/1023;
     v3 = v3 * 48/5 - 24;
-    Serial.println("VDC4:" + String(v3, 2));
+    Serial.println(String(v3, 2) + "Vdc");
+    display.setCursor(0, 36);
+    display.print("V4: ");
+    display.print(String(v3, 2));
+    display.print(" Vdc");
   }
+  display.display();
   //delay(1000);
 }
